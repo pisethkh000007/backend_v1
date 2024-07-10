34233055777d138092302dbab1218b443864f735
virak <virakson444@gmail.com>
2024-07-10 15:01:44 +0700

    update

--- jest.setup.js
+++ jest.setup.js
@@ -1,7 +1,2 @@
-// const path = require('path');
-// require('dotenv').config({ path: path.resolve(__dirname, './src/configs/.env.development') });
-const dotenv = require("dotenv");
-const path = require("path");
-
-const envPath = path.resolve(__dirname, "src/configs/.env.development");
-dotenv.config({ path: envPath });
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, './src/configs/.env.development') });
