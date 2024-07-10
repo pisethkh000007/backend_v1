7d12d9f0bad5a8e1fa44f710acf00c63821f8c00
virak <virakson444@gmail.com>
2024-07-10 15:33:24 +0700

    update

--- jest.setup.js
+++ jest.setup.js
@@ -1,2 +1,14 @@
-const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, './src/configs/.env.development') });
+// const path = require('path');
+// require('dotenv').config({ path: path.resolve(__dirname, './src/configs/.env.development') });
+
+const dotenv = require("dotenv");
+const path = require("path");
+
+const envPath = path.resolve(__dirname, "src/configs/.env.development");
+const result = dotenv.config({ path: envPath });
+
+if (result.error) {
+  console.error("Error loading .env file:", result.error);
+} else {
+  console.log("Environment variables loaded successfully:", process.env);
+}
