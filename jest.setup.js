94cdb5ef9f38b82427be8f488fc83869dbd53cc0
virak <virakson444@gmail.com>
2024-07-21 15:51:19 +0700

    update

--- jest.setup.js
+++ jest.setup.js
@@ -1,22 +1,14 @@
-// const path = require('path');
-// require('dotenv').config({ path: path.resolve(__dirname, './src/configs/.env.development') });
+const dotenv = require("dotenv");
+const path = require("path");
 
-// const dotenv = require("dotenv");
-// const path = require("path");
+const envPath = path.resolve(__dirname, "./src/configs/.env.test");
+const result = dotenv.config({ path: envPath });
 
-// const envPath = path.resolve(__dirname, "src/configs/.env.development");
-// const result = dotenv.config({ path: envPath });
-
-// if (result.error) {
-//   console.error("Error loading .env file:", result.error);
-// } else {
-//   console.log("Environment variables loaded successfully:", process.env);
-// }
-
-process.env.PORT = "3000";
-process.env.MONGODB_URL =
-  "mongodb+srv://virakson444:NfT5ug3pATwLxQxQ@bootcamp-project.lwadf47.mongodb.net/auths";
-
-console.log("MONGODB_URL:", process.env.MONGODB_URL);
-console.log("NODE_ENV:", process.env.NODE_ENV);
-console.log("PORT:", process.env.PORT);
+if (result.error) {
+  console.error("Error loading .env.test file:", result.error);
+} else {
+  console.log("Environment variables loaded successfully:");
+  console.log("MONGODB_URL:", process.env.MONGODB_URL);
+  console.log("NODE_ENV:", process.env.NODE_ENV);
+  console.log("PORT:", process.env.PORT);
+}
