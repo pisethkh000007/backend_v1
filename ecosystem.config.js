01be02ef013cca4071fad32b56fd1289afdf19c1
virak <virakson444@gmail.com>
2024-07-22 15:28:30 +0700

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,12 +2,17 @@ module.exports = {
   apps: [
     {
       name: "my-app",
-      script: "build/server.js", // Adjust this path if necessary
-      env_production: {
-        NODE_ENV: "production",
+      script: "build/server.js",
+      env: {
+        NODE_ENV: "development",
         PORT: 3000,
         MONGODB_URL:
           "mongodb+srv://virakson444:12345@bootcamp-project.lwadf47.mongodb.net/auths",
+      },
+      env_production: {
+        NODE_ENV: "production",
+        PORT: process.env.PORT,
+        MONGODB_URL: process.env.MONGODB_URL,
       },
     },
   ],
