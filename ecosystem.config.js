c85eb5bccb6492b9f58177b31ecfc49fc98b0fa7
Son Virakchantrak <120432101+Binhod07@users.noreply.github.com>
2024-07-23 12:47:53 +0700

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,3 +1,10 @@
+const path = require("path");
+const dotenv = require("dotenv");
+
+// Load environment variables from .env.production file
+const envPath = path.resolve(__dirname, "src/configs/.env.production");
+dotenv.config({ path: envPath });
+
 module.exports = {
   apps: [
     {
@@ -6,8 +13,7 @@ module.exports = {
       env: {
         NODE_ENV: "development",
         PORT: 3000,
-        MONGODB_URL:
-          "mongodb+srv://virakson444:12345@bootcamp-project.lwadf47.mongodb.net/auths",
+        MONGODB_URL: "mongodb+srv://virakson444:12345@bootcamp-project.lwadf47.mongodb.net/auths",
       },
       env_production: {
         NODE_ENV: "production",
